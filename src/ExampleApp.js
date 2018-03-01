f8be2e200b6821ed0ac6b7350ad977645f2f6cc5
Nathan Dunn <ndunn@me.com>
2018-03-01 13:49:39 -0800

    set data per state

--- src/ExampleApp.js
+++ src/ExampleApp.js
@@ -15,8 +15,19 @@ export default class SampleApp extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
-            hoverData: {
+            pathwayData: {
+                expression: ExampleExpression,
+                pathways: ExamplePathWays,
+                samples: ExampleSamples,
+            },
+            geneData: {
+                expression: [],
+                pathways: [],
+                samples: [],
+            },
+            pathwayHoverData: {
                 aaa: 'aaaaValue',
                 bbb: 'bbbbValue',
                 x: null,
@@ -25,7 +36,7 @@ export default class SampleApp extends Component {
                 pathway: null,
                 score: null
             },
-            clickData: {
+            pathwayClickData: {
                 aaa: 'aaaaValue',
                 bbb: 'bbbbValue',
                 x: null,
@@ -33,40 +44,64 @@ export default class SampleApp extends Component {
                 tissue: null,
                 pathway: null,
                 score: null
-            }
+            },
+            geneHoverData: {
+                aaa: 'aaaaValue',
+                bbb: 'bbbbValue',
+                x: null,
+                y: null,
+                tissue: null,
+                gene: null,
+                score: null
+            },
+            geneClickData: {
+                aaa: 'aaaaValue',
+                bbb: 'bbbbValue',
+                x: null,
+                y: null,
+                tissue: null,
+                pathway: null,
+                score: null
+            },
+
         };
 
         this.hoverPathway = this.hoverPathway.bind(this);
         this.clickPathway = this.clickPathway.bind(this);
+        this.hoverGene = this.hoverGene.bind(this);
+        this.clickGene = this.clickGene.bind(this);
 
 
     }
 
     clickPathway(props) {
         if (props && props.x) {
-            this.setState({clickData: props});
+            this.setState({pathwayClickData: props});
+            this.setState({geneData: this.state.pathwayData});
+
+            console.log(this.state.pathwayClickData)
         }
     }
 
     hoverPathway(props) {
         if (props && props.x) {
-            this.setState({hoverData: props});
+            this.setState({pathwayHoverData: props});
+        }
+    }
+
+    clickGene(props) {
+        if (props && props.x) {
+            this.setState({geneClickData: props});
+        }
+    }
+
+    hoverGene(props) {
+        if (props && props.x) {
+            this.setState({geneHoverData: props});
         }
     }
 
     render() {
-        let data = {
-            expression: ExampleExpression,
-            pathways: ExamplePathWays,
-            samples: ExampleSamples,
-        };
-
-        // we will change this on the fly, I think.
-        let geneData = {
-            expression: ExampleExpression,
-            pathways: ExamplePathWays,
-            samples: ExampleSamples,
-        };
 
         const alignTop = {
             marginLeft: '40px',
@@ -87,26 +122,30 @@ export default class SampleApp extends Component {
         return (
             <div>
                 <table>
+                    <tbody>
                     <tr>
                         <td>
                             <h2>Cohorts</h2>
                             <CohortSelector cohorts={ExampleCohortsData}/>
-                            <TissueView id="pathwayViewId" width="400" height="800" data={data}
+                            <TissueView id="pathwayViewId" width="400" height="800" data={this.state.pathwayData}
                                         onClick={this.clickPathway} onHover={this.hoverPathway}/>
                         </td>
                         <td style={alignTop}>
-                            <HoverView title="Hover" data={this.state.hoverData}/>
-                            <HoverView title="Clicked" data={this.state.clickData}/>
+                            <HoverView title="Hover" data={this.state.pathwayHoverData}/>
+                            <HoverView title="Clicked" data={this.state.pathwayClickData}/>
                         </td>
-                        <td style={geneAlignment}>
-                            <TissueView id="geneViewId" width="400" height="800" data={geneData}
-                                        onClick={this.clickPathway} onHover={this.hoverPathway}/>
-                        </td>
+                        { this.state.geneData && this.state.geneData.expression.rows && this.state.geneData.expression.rows.length > 0 &&
+                            <td style={geneAlignment}>
+                                <TissueView id="geneViewId" width="400" height="800" data={this.state.geneData}
+                                            onClick={this.clickGene} onHover={this.hoverGene}/>
+                            </td>
+                        }
                         <td style={alignTop}>
-                            <HoverView title="Hover" data={this.state.hoverData}/>
-                            <HoverView title="Clicked" data={this.state.clickData}/>
+                            <HoverView title="Hover" data={this.state.geneHoverData}/>
+                            <HoverView title="Clicked" data={this.state.geneClickData}/>
                         </td>
                     </tr>
+                    </tbody>
                 </table>
             </div>
         );
