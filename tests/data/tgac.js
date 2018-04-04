aea418c1b088f4790cbae9269d9d4f3b6b1a742e
jingchunzhu <jingchunzhu@gmail.com>
2018-04-04 16:03:24 -0700

    Update tgac.js

--- tests/data/tgac.js
+++ tests/data/tgac.js
@@ -310,13 +310,13 @@ export default [
             "AEN", "URS000039ED8D_9606", "RFFL", "DYRK2"]
     },
     {
-        "goid": "cell cycle (Pancan Atlas)",
+        "goid": "",
         "golabel": "cell cycle (Pancan Atlas)",
         "gene": ["CDKN1A","CDKN1B","CDKN2A","CDKN2B","CDKN2C","CCND1","CCND2","CCND3","CCNE1",
             "CDK2","CDK4","CDK6","RB1","E2F1","E2F3"]
     },
     {
-        "goid": "HIPPO (Pancan Atlas)",
+        "goid": "",
         "golabel": "HIPPO (Pancan Atlas)",
         "gene":["STK4","STK3","SAV1","LATS1","LATS2","MOB1A","MOB1B","YAP1","WWTR1","TEAD1",
             "TEAD2","TEAD3","TEAD4","PTPN14","NF2","WWC1","TAOK1","TAOK2","TAOK3","CRB1","CRB2",
@@ -324,13 +324,13 @@ export default [
             "DCHS2","CSNK1E","CSNK1D","AJUBA","LIMD1","WTIP"]
     },
     {
-        "goid": "MYC (Pancan Atlas)",
+        "goid": "",
         "golabel": "MYC (Pancan Atlas)",
         "gene":["MAX","MGA","MLX","MLXIP","MLXIPL","MNT","MXD1","MXD3","MXD4","MXI1","MYC",
             "MYCL","MYCN"]
     },
     {
-        "goid": "NOTCH (Pancan Atlas)",
+        "goid": "",
         "golabel": "NOTCH (Pancan Atlas)",
         "gene":["ARRDC1","CNTN6","CREBBP","EP300","HES1","HES2","HES3","HES4","HES5","HEY1",
             "HEY2","HEYL","KAT2B","KDM5A","NOTCH1","NOTCH2","NOTCH3","NOTCH4","NOV","NRARP",
@@ -341,24 +341,24 @@ export default [
             "DLL3","DLL4","DNER","DTX1","DTX2","DTX3","DTX3L","DTX4","EGFL7"]
     },
     {
-        "goid": "NRF2 (Pancan Atlas)",
+        "goid": "",
         "golabel": "NRF2 (Pancan Atlas)",
         "gene":["NFE2L2", "KEAP1","CUL3"]
     },
     {
-        "goid": "PI3K (Pancan Atlas)",
+        "goid": "",
         "golabel": "PI3K (Pancan Atlas)",
         "gene":["EIF4EBP1","AKT1","AKT2","AKT3","AKT1S1","DEPDC5","DEPTOR","INPP4B","MAPKAP1",
             "MLST8","MTOR","NPRL2","NPRL3","PDK1","PIK3CA","PIK3CB","PIK3R1","PIK3R2","PIK3R3",
             "PPP2R1A","PTEN","RHEB","RICTOR","RPTOR","RPS6","RPS6KB1","STK11","TSC1","TSC2"]
     },
     {
-        "goid": "TGF-Beta (Pancan Atlas)",
+        "goid": "",
         "golabel": "TGF-Beta (Pancan Atlas)",
         "gene":["TGFBR1","TGFBR2","ACVR2A","ACVR1B","SMAD2","SMAD3","SMAD4"]
     },
     {
-        "goid": "RTK RAS (Pancan Atlas)",
+        "goid": "",
         "golabel": "RTK RAS (Pancan Atlas)",
         "gene": ["ABL1","EGFR","ERBB2","ERBB3","ERBB4","PDGFRA","PDGFRB","MET","FGFR1","FGFR2",
             "FGFR3","FGFR4","FLT3","ALK","RET","ROS1","KIT","IGF1R","NTRK1","NTRK2","NTRK3",
@@ -371,12 +371,12 @@ export default [
             "PEA15","JAK2","IRS2"]
     },
     {
-        "goid": "TP53 (Pancan Atlas)",
+        "goid": "",
         "golabel": "TP53 (Pancan Atlas)",
         "gene":["TP53","MDM2","MDM4","ATM","CHEK2","RPS6KA3"]
     },
     {
-        "goid": "WNT (Pancan Atlas)",
+        "goid": "",
         "golabel": "WNT (Pancan Atlas)",
         "gene":["CHD8","LEF1","LGR4","LGR5","LRP5","LRP6","LZTR1","NDP","PORCN","RSPO1",
             "SFRP1","SFRP2","SFRP4","SFRP5","SOST","TCF7L1","TLE1","TLE2","TLE3","TLE4","WIF1",
