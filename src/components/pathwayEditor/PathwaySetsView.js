744242203437110584aa9b382b21c7bde6f82b3c
Nathan Dunn <ndunn@me.com>
2018-06-01 14:10:29 -0700

    added a view default button

--- src/components/pathwayEditor/PathwaySetsView.js
+++ src/components/pathwayEditor/PathwaySetsView.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import {Button} from 'react-toolbox/lib/button';
 import {Grid, Row, Col} from 'react-material-responsive-grid';
 import FaTrash from 'react-icons/lib/fa/trash';
+import FaEye from 'react-icons/lib/fa/eye';
 
 
 export default class PathwaySetsView extends PureComponent {
@@ -19,14 +20,24 @@ export default class PathwaySetsView extends PureComponent {
                     <Col md={8}>
                         <Button primary>{pathway.name}</Button>
                     </Col>
-                    <Col md={2}>
-                        <Button><FaTrash/></Button>
+                    <Col md={1}>
+                        <a role='button' onClick={()=>this.view(pathway)}><FaEye/></a>
+                    </Col>
+                    <Col md={1}>
+                        <a role='button' onClick={()=>this.deleteView(pathway)}><FaTrash/></a>
                     </Col>
                 </Row>
             )
         });
     }
 
+    view(pathway) {
+        alert('viewing: '+JSON.stringify(pathway))
+    }
+
+    deleteView(pathway) {
+        alert('delete view: '+JSON.stringify(pathway))
+    }
 }
 
 
