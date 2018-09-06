c95334d031467b78d759162bc5c11483ecbe4276
Nathan Dunn <ndunn@me.com>
2018-09-06 14:40:43 -0700

    refactored to get the proper pathways display on that

--- src/components/GeneSetSvgSelector.js
+++ src/components/GeneSetSvgSelector.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import PureComponent from './PureComponent';
 import PropTypes from 'prop-types';
-import {pick, groupBy, mapObject, pluck, flatten} from 'underscore';
-import {sum} from '../functions/util';
-import {Dropdown} from "react-toolbox";
-import {getCopyNumberValue} from "../functions/DataFunctions";
+import {fontColor, getSelectColor, getHoverColor} from "../functions/ColorFunctions";
 
 
 export class GeneSetSvgSelector extends PureComponent {
@@ -17,24 +14,118 @@ export class GeneSetSvgSelector extends PureComponent {
         };
     }
 
-    render() {
-        let {pathways} = this.props;
-        return (
-            <div style={{marginLeft: 10, marginTop: 0}}>
-                Gene Set Selector
-                {pathways &&
-                    pathways.map( (p) => {
-                        console.log(p)
-                        return (
-                            <div>
-                                {p.golabel}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        );
+    /**
+     * Score is from 0 to 1
+     * @param score
+     * @param selected
+     * @param hovered
+     * @param labelOffset
+     * @param left
+     * @param width
+     * @param labelHeight
+     * @param colorMask
+     * @returns {*}
+     */
+    labelStyle(score, selected, hovered, labelOffset, left, width, labelHeight, colorMask) {
+
+        let colorString = 'rgba(';
+        colorString += colorMask[0];
+        colorString += ',';
+        colorString += colorMask[1];
+        colorString += ',';
+        colorString += colorMask[2];
+        colorString += ',';
+        colorString += score + ')';
+
+        // console.log('SH',selected,hovered)
+        if (selected) {
+            return {
+                position: 'absolute',
+                top: labelOffset,
+                left: left,
+                height: labelHeight,
+                width: width,
+                backgroundColor: getSelectColor(),
+                strokeWidth: 1,
+                cursor: 'pointer'
+            }
+        }
+
+        else if (hovered) {
+            return {
+                position: 'absolute',
+                top: labelOffset,
+                left: left,
+                height: labelHeight,
+                width: width,
+                backgroundColor: getHoverColor(score),
+                strokeWidth: 1,
+                // outline: 'thin dotted gray',
+                borderRadius: '15px',
+                boxShadow: '0 0 1px 1px gray',
+                cursor: 'pointer'
+            }
+        }
+        else {
+            return {
+                // position: 'absolute',
+                top: labelOffset,
+                left: left,
+                height: labelHeight,
+                width: width,
+                backgroundColor: colorString,
+                strokeWidth: 2,
+                cursor: 'pointer'
+            }
+        }
     }
+
+    render() {
+        let {pathways, selected, hovered, width, labelString, labelHeight, item, geneLength, highScore, labelOffset, left, colorMask} = this.props;
+        let colorDensity = 0.5;
+        labelHeight = 20;
+        let labelWidget = 150;
+        let className = 'asdf';
+        colorMask = [0.5, 0.5, 0.5];
+        // labelOffset = 0;
+
+
+
+        return pathways.map((p, index) => {
+            let labelString = p.golabel;
+            return (
+                <svg
+                    style={this.labelStyle(colorDensity, selected, hovered, labelOffset, left, width, labelHeight, colorMask)}
+                    className={className}
+                    key={p.golabel}
+                >
+                    <text x={10} y={10} fontFamily='Arial' fontSize={10}
+                          fill={fontColor(colorDensity, selected, hovered)}
+                    >
+                        {width < 10 ? '' : labelString}
+                    </text>
+                </svg>
+            );
+        });
+    }
+
+    // render() {
+    //     let {width, labelString, labelHeight, item, geneLength, highScore} = this.props;
+    //     let className = (item.gene.length === 1 ? item.gene[0] : item.golabel).replace(/ /g, '-');
+    //     let colorDensity = this.getColorDensity(item.density, geneLength, highScore);
+    //     return (
+    //         <svg
+    //             style={this.style(colorDensity)}
+    //             className={className}
+    //         >
+    //             <text x={-labelHeight + 2} y={10} fontFamily='Arial' fontSize={10} fill={this.fontColor(colorDensity)}
+    //                   transform='rotate(-90)'
+    //             >
+    //                 {width < 10 ? '' : labelString}
+    //             </text>
+    //         </svg>
+    //     );
+    // }
 }
 
 GeneSetSvgSelector.propTypes = {
