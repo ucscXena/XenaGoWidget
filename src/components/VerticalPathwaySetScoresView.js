7243a8c7ac9846b11eaf2799476519c93dfd1fb9
Nathan Dunn <ndunn@me.com>
2018-11-27 11:59:33 -0800

    things are showing upm hyst bit quite drawing correctly

--- src/components/VerticalPathwaySetScoresView.js
+++ src/components/VerticalPathwaySetScoresView.js
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 
 import DrawFunctions from '../functions/DrawFunctions';
 import CanvasDrawing from "../CanvasDrawing";
+import {findAssociatedData, findPruneData} from '../functions/DataFunctions';
+import {hierarchicalSort, clusterSort, synchronizedSort} from '../functions/SortFunctions';
+import {pick, pluck, flatten, isEqual} from 'underscore';
+import {FILTER_PERCENTAGE, LABEL_A, LABEL_B} from "./XenaGeneSetApp";
 
 /**
  * Extends PathwaysScoreView (but the old one)
@@ -14,12 +18,18 @@ export default class VerticalPathwaySetScoresView extends PureComponent {
         super(props);
     }
 
+    // TODO: restructure
+    getGenesForPathways(pathways) {
+        return Array.from(new Set(flatten(pluck(pathways, 'gene'))));
+    };
+
     render(){
 
-        const { data, cohortIndex, labelHeight, width} = this.props ;
+        let { data, cohortIndex, filter, selectedSort, labelHeight, width} = this.props ;
+        const {expression, pathways, samples, copyNumber, referencePathways} = data;
         console.log('input data,',data)
         if(!data || !data.pathways){
-            return <div>No data {cohortIndex}</div>
+            return <div>Loading Cohort {cohortIndex === 0 ? LABEL_A : LABEL_B }</div>
         }
         // need a size and vertical start for each
         let layout = data.pathways.map( ( p , index ) => {
@@ -28,6 +38,60 @@ export default class VerticalPathwaySetScoresView extends PureComponent {
         console.log('layout',layout)
         const totalHeight = layout.length *  labelHeight ;
         // let layoutData = layout(width, returnedValue.data);
+
+
+        let geneList = this.getGenesForPathways(pathways);
+
+
+        // TODO: fix sort somehow
+
+        // TODO: fix filter somehow?
+        filter = filter ? filter : 'All';
+        selectedSort = selectedSort ? selectedSort : 'Cluster';
+
+        // call
+
+        // need to get an associatedData
+        let hashAssociation = {
+            expression,
+            copyNumber,
+            geneList,
+            pathways,
+            samples,
+            // filter,
+            filter,
+            // min,
+            cohortIndex,
+            // selectedCohort
+        };
+        if (expression === undefined || expression.length === 0) {
+            return <div>Loading...</div>
+        }
+        let associatedData = findAssociatedData(hashAssociation);
+        let filterMin = Math.trunc(FILTER_PERCENTAGE * samples.length);
+
+        let hashForPrune = {
+            associatedData,
+            pathways,
+            filterMin
+        };
+        let prunedColumns = findPruneData(hashForPrune);
+        prunedColumns.samples = samples;
+        let returnedValue;
+
+        switch (selectedSort) {
+            case 'Hierarchical':
+                returnedValue = hierarchicalSort(prunedColumns);
+                break;
+            case 'Cluster':
+            default:
+                returnedValue = clusterSort(prunedColumns);
+                break;
+        }
+
+
+
+
         return (
             <div>
                 <CanvasDrawing
@@ -38,7 +102,14 @@ export default class VerticalPathwaySetScoresView extends PureComponent {
                     cohortIndex={cohortIndex}
                     labelHeight={labelHeight}
                     height={totalHeight}
-                    data={data}
+                    associatedData={returnedValue.data}
+                    data={{
+                        expression,
+                        pathways: returnedValue.pathways,
+                        referencePathways,
+                        samples,
+                        sortedSamples: returnedValue.sortedSamples
+                    }}
                 />
             </div>
         )
--- src/functions/DrawFunctions.js
+++ src/functions/DrawFunctions.js
@@ -76,41 +76,52 @@ function drawExpressionData(ctx, width, totalHeight, layout, data, labelHeight, 
 /**
  * TODO: handle for other type
  * @param index
- * @param height
+ * @param width
  * @param count
  */
-function findPathwayRegions(index, height, count) {
+function findPathwayRegions(index, width, count) {
     // Find pixel regions having the same set of samples, e.g.
     // 10 samples in 1 px, or 1 sample over 10 px. Record the
     // range of samples in the region.
     let regions = reduceByKey(range(count),
-        i => ~~(i * height / count),
-        (i, y, r) => r ? {...r, end: i} : {y, start: i, end: i});
+        i => ~~(i * width / count),
+        (i, x, r) => r ? {...r, end: i} : {x, start: i, end: i});
     let starts = Array.from(regions.keys());
-    let se = partitionN(starts, 2, 1, [height]);
+    let se = partitionN(starts, 2, 1, [width]);
 
     // XXX side-effecting map
-    map2(starts, se, (start, [s, e]) => regions.get(start).height = e - s);
+    map2(starts, se, (start, [s, e]) => regions.get(start).width = e - s);
 
     return regions;
 }
 
-function drawPathwayData(ctx, width, totalHeight, layout, data, labelHeight, colorMask, cohortIndex) {
-    let height = totalHeight - labelHeight;
-    let tissueCount = data[0].length;
-    let regions = findPathwayRegions(0, height, tissueCount);
+
+function drawPathwayStub(ctx, width, totalHeight, layout, data, labelHeight, colorMask, cohortIndex) {
+    // let height = totalHeight - labelHeight;
+    console.log('height: ',totalHeight,layout.length,labelHeight)
+    console.log('input length: ',data)
+    let tissueCount = data.length;
+    let regions = findPathwayRegions(0, width, tissueCount);
+    console.log('regions',regions)
     let img = ctx.createImageData(width, totalHeight);
 
-    let offsetHeight = cohortIndex === 0 ? 0 : labelHeight;
+    // let offsetHeight = cohortIndex === 0 ? 0 : labelHeight;
+    // ctx.save();
+    // ctx.fillStyle = '#333333'; // sets the color to fill in the rectangle with
+    // ctx.strokeStyle = '#AAAAAA'; // sets the color to fill in the rectangle with
+    // console.log('width',width);
+    // console.log('totalHeight',totalHeight);
+    // ctx.fillRect(0, 0, width, totalHeight);
 
     layout.forEach(function (el, i) {
-        // TODO: may be faster to transform the whole data cohort at once
+    //     // TODO: may be faster to transform the whole data cohort at once
         let rowData = data[i];
+        // console.log('row data',rowData)
         if (cohortIndex === 0) {
             rowData = data[i].reverse();
         }
 
-        // let reverseMap = new Map(Array.from(regions).reverse());
+        let offsetHeight = 0 ;
         // XXX watch for poor iterator performance in this for...of.
         for (let rs of regions.keys()) {
             let r = regions.get(rs);
@@ -118,8 +129,10 @@ function drawPathwayData(ctx, width, totalHeight, layout, data, labelHeight, col
 
             let color = regionColor(d);
 
-            for (let y = rs + offsetHeight; y < rs + r.height + offsetHeight; ++y) {
-                let pxRow = y * width,
+            // console.log('setting for color', rs, r)
+            for (let x = rs + offsetHeight; x < rs + r.width + offsetHeight; ++x) {
+                // console.log('in y, etc.',x)
+                let pxRow = x * width,
                     buffStart = (pxRow + el.start) * 4,
                     buffEnd = (pxRow + el.start + el.size) * 4;
                 for (let l = buffStart; l < buffEnd; l += 4) {
@@ -130,57 +143,9 @@ function drawPathwayData(ctx, width, totalHeight, layout, data, labelHeight, col
                 }
             }
         }
-
+    //
         ctx.putImageData(img, 0, 0);
     });
-}
-
-function drawPathwayStub(ctx, width, totalHeight, layout, data, labelHeight, colorMask, cohortIndex) {
-    // let height = totalHeight - labelHeight;
-    console.log('height: ',totalHeight,layout.length,labelHeight)
-    console.log('input length: ',data)
-    let tissueCount = data.samples.length;
-    // let regions = findPathwayRegions(0, height, tissueCount);
-    // let img = ctx.createImageData(width, totalHeight);
-
-    // let offsetHeight = cohortIndex === 0 ? 0 : labelHeight;
-    ctx.save();
-    ctx.fillStyle = '#333333'; // sets the color to fill in the rectangle with
-    ctx.strokeStyle = '#AAAAAA'; // sets the color to fill in the rectangle with
-    console.log('width',width);
-    console.log('totalHeight',totalHeight);
-    ctx.fillRect(0, 0, width, totalHeight);
-
-    // layout.forEach(function (el, i) {
-    //     // TODO: may be faster to transform the whole data cohort at once
-    //     let rowData = data[i];
-    //     if (cohortIndex === 0) {
-    //         rowData = data[i].reverse();
-    //     }
-    //
-    //     // let reverseMap = new Map(Array.from(regions).reverse());
-    //     // XXX watch for poor iterator performance in this for...of.
-    //     for (let rs of regions.keys()) {
-    //         let r = regions.get(rs);
-    //         let d = rowData.slice(r.start, r.end + 1);
-    //
-    //         let color = regionColor(d);
-    //
-    //         for (let y = rs + offsetHeight; y < rs + r.height + offsetHeight; ++y) {
-    //             let pxRow = y * width,
-    //                 buffStart = (pxRow + el.start) * 4,
-    //                 buffEnd = (pxRow + el.start + el.size) * 4;
-    //             for (let l = buffStart; l < buffEnd; l += 4) {
-    //                 img.data[l] = colorMask[0];
-    //                 img.data[l + 1] = colorMask[1];
-    //                 img.data[l + 2] = colorMask[2];
-    //                 img.data[l + 3] = color;
-    //             }
-    //         }
-    //     }
-    //
-    //     ctx.putImageData(img, 0, 0);
-    // });
 }
 
 
@@ -201,7 +166,7 @@ export default {
     },
 
     drawPathwayView(vg, props) {
-        let {width, layout, labelHeight, cohortIndex, data} = props;
+        let {width, layout, labelHeight, cohortIndex, associatedData} = props;
 
         console.log('draw pathway view',props)
         let totalHeight = labelHeight * layout.length;
@@ -212,7 +177,7 @@ export default {
         // }
         //
         // drawPathwayData(vg, width, height, layout, associateData, GENE_LABEL_HEIGHT, getPathwayColorMask(), cohortIndex);
-        drawPathwayStub(vg, width, totalHeight,layout, data, labelHeight, getPathwayColorMask(), cohortIndex);
+        drawPathwayStub(vg, width, totalHeight,layout, associatedData, labelHeight, getPathwayColorMask(), cohortIndex);
 
     },
 
