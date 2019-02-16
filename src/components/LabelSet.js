3ff6cd8a215af587f2a7fbf9b1bf0c8f14a76fed
Nathan Dunn <ndunn@me.com>
2019-02-15 18:21:45 -0800

    create the label set properly

--- src/components/LabelSet.js
+++ src/components/LabelSet.js
@@ -21,61 +21,25 @@ export default class LabelSet extends PureComponent {
         super(props);
     }
 
-    drawOverviewLabels(width, height, layout, pathways, selectedPathways, hoveredPathways, labelHeight, labelOffset, colorMask, cohortIndex, highlightedGene,shadingValue) {
-
-        if (layout[0].size <= 1) {
-            return;
-        }
-
-        const numSamples = this.props.data.samples.length;
-        if (pathways.length === layout.length) {
-            return layout.map((el, i) => {
-                let d = pathways[i];
-                let geneLength = d.gene.length;
-                let hovered, selected;
-                let labelKey = d.gene[0];
-                let labelString = labelKey; // can this go away?
-                hovered = hoveredPathways.indexOf(d.gene[0]) >= 0;
-                selected = selectedPathways.indexOf(labelString) >= 0;
-                let highlighted = highlightedGene === labelKey;
-                return (
-                    <HeaderLabel
-                        labelHeight={labelHeight}
-                        labelOffset={labelOffset}
-                        numSamples={numSamples}
-                        geneLength={geneLength}
-                        left={el.start}
-                        width={el.size}
-                        item={d}
-                        selected={selected}
-                        hovered={hovered}
-                        highlighted={highlighted}
-                        labelString={labelString}
-                        colorMask={colorMask}
-                        key={labelKey + '-' + cohortIndex}
-                        shadingValue={shadingValue}
-                    />
-                )
-            });
-        }
-    }
-
-    drawTissueOverlay() {
-        let {shadingValue,geneLabelHeight, width, height, layout, associateData, cohortIndex, hoveredPathways, highlightedGene, data: {pathways}} = this.props;
-
-        if (associateData.length === 0) {
-            return;
-        }
-
-        // draw genes
-        let offset = cohortIndex === 0 ? height - geneLabelHeight : 0;
-        return this.drawOverviewLabels(width, height, layout, pathways, [], hoveredPathways, geneLabelHeight, offset, getGeneColorMask(), cohortIndex, highlightedGene,shadingValue);
-    }
-
     render() {
-        const {associateData,pathways,layout,hoveredPathways,selectedPathways,highlightedGene,labelHeight,labelOffset,numSamples,colorMask,cohortIndex,shadingValue} = this.props;
+        const {
+            associateData
+            , pathways
+            , layout
+            , hoveredPathways
+            , selectedPathways
+            , highlightedGene
+            , labelHeight
+            , labelOffset
+            , colorMask
+            , cohortIndex
+            , shadingValue
+            , data
+        } = this.props;
         // let offset = cohortIndex === 0 ? height - geneLabelHeight : 0;
-        if (associateData.length >0 && pathways.length === layout.length) {
+        console.log(this.props)
+        if (data && data.samples && associateData && associateData.length > 0 && pathways.length === layout.length) {
+            const numSamples = data.samples.length;
             return layout.map((el, i) => {
                 let d = pathways[i];
                 let geneLength = d.gene.length;
@@ -105,18 +69,22 @@ export default class LabelSet extends PureComponent {
                 )
             });
         }
-        else{
-            return '<div></div>';
+        else {
+            return <div></div>;
         }
     }
 }
 LabelSet.propTypes = {
-    width: PropTypes.any.isRequired,
-    height: PropTypes.any.isRequired,
-    offset: PropTypes.any.isRequired,
-    onClick: PropTypes.any.isRequired,
-    onMouseMove: PropTypes.any.isRequired,
-    onMouseOut: PropTypes.any.isRequired,
-    geneLabelHeight: PropTypes.any.isRequired,
+    associateData: PropTypes.any.isRequired,
+    pathways: PropTypes.any.isRequired,
+    data: PropTypes.any.isRequired,
+    layout: PropTypes.any.isRequired,
+    hoveredPathways: PropTypes.any.isRequired,
+    selectedPathways: PropTypes.any.isRequired,
+    // highlightedGene: PropTypes.any.isRequired,
+    labelHeight: PropTypes.any.isRequired,
+    labelOffset: PropTypes.any.isRequired,
+    colorMask: PropTypes.any.isRequired,
     cohortIndex: PropTypes.any.isRequired,
+    shadingValue: PropTypes.any.isRequired,
 };
--- src/components/LabelWrapper.js
+++ src/components/LabelWrapper.js
@@ -1,7 +1,6 @@
 import PureComponent from "./PureComponent";
 import PropTypes from 'prop-types';
 import React from 'react'
-import {HeaderLabel} from "../components/HeaderLabel";
 import {getGeneColorMask} from '../functions/ColorFunctions'
 import LabelSet from "./LabelSet";
 
@@ -22,59 +21,27 @@ export default class LabelWrapper extends PureComponent {
         super(props);
     }
 
-    drawOverviewLabels(width, height, layout, pathways, selectedPathways, hoveredPathways, labelHeight, labelOffset, colorMask, cohortIndex, highlightedGene,shadingValue) {
-
-        if (layout[0].size <= 1) {
-            return;
-        }
-
-        const numSamples = this.props.data.samples.length;
-        if (pathways.length === layout.length) {
-            return layout.map((el, i) => {
-                let d = pathways[i];
-                let geneLength = d.gene.length;
-                let hovered, selected;
-                let labelKey = d.gene[0];
-                let labelString = labelKey; // can this go away?
-                hovered = hoveredPathways.indexOf(d.gene[0]) >= 0;
-                selected = selectedPathways.indexOf(labelString) >= 0;
-                let highlighted = highlightedGene === labelKey;
-                return (
-                    <HeaderLabel
-                        labelHeight={labelHeight}
-                        labelOffset={labelOffset}
-                        numSamples={numSamples}
-                        geneLength={geneLength}
-                        left={el.start}
-                        width={el.size}
-                        item={d}
-                        selected={selected}
-                        hovered={hovered}
-                        highlighted={highlighted}
-                        labelString={labelString}
-                        colorMask={colorMask}
-                        key={labelKey + '-' + cohortIndex}
-                        shadingValue={shadingValue}
-                    />
-                )
-            });
-        }
-    }
-
-    drawTissueOverlay() {
-        let {shadingValue,geneLabelHeight, width, height, layout, associateData, cohortIndex, hoveredPathways, highlightedGene, data: {pathways}} = this.props;
-
-        if (associateData.length === 0) {
-            return;
-        }
-
-        // draw genes
-        let offset = cohortIndex === 0 ? height - geneLabelHeight : 0;
-        return this.drawOverviewLabels(width, height, layout, pathways, [], hoveredPathways, geneLabelHeight, offset, getGeneColorMask(), cohortIndex, highlightedGene,shadingValue);
-    }
 
     render() {
-        const {width, height, onClick, onMouseMove, onMouseOut, offset} = this.props;
+        let {
+            shadingValue
+            , geneLabelHeight
+            , width
+            , height
+            , layout
+            , onClick
+            , onMouseMove
+            , onMouseOut
+            , associateData
+            , cohortIndex
+            , hoveredPathways
+            , highlightedGene
+            , data: {pathways}
+        } = this.props;
+
+
+        let offset = cohortIndex === 0 ? height - geneLabelHeight : 0;
+        console.log('offset ',offset)
 
 
         return (
@@ -83,7 +50,20 @@ export default class LabelWrapper extends PureComponent {
                  onMouseOut={onMouseOut}
                  onClick={onClick}
             >
-                <LabelSet width={} height={} offset={} onClick={} onMouseMove={} onMouseOut={} geneLabelHeight={} cohortIndex={}/>
+                <LabelSet
+                    associateData={associateData}
+                    pathways={pathways}
+                    layout={layout}
+                    hoveredPathways={hoveredPathways}
+                    selectedPathways={[]}
+                    highlightedGene={highlightedGene}
+                    labelHeight={geneLabelHeight}
+                    labelOffset={offset}
+                    colorMask={getGeneColorMask()}
+                    cohortIndex={cohortIndex}
+                    shadingValue={shadingValue}
+                    data={this.props.data}
+                />
             </div>
         )
     }
