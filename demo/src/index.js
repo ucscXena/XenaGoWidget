536f1f772fc702b65345ef81f3076dec1d592398
Nathan Dunn <ndunn@me.com>
2019-03-08 15:59:15 -0800

    added an appropriate title

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,6 +3,7 @@ import {render} from 'react-dom'
 
 import PureComponent from "../../src/components/PureComponent";
 import XenaGeneSetApp from "../../src/components/XenaGeneSetApp";
+import {Helmet} from "react-helmet";
 
 
 class Demo extends PureComponent {
@@ -10,6 +11,10 @@ class Demo extends PureComponent {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>Xena Gene Set Viewer</title>
+                    <meta name="description" content="Xena Gene Set Viewer" />
+                </Helmet>
                 <XenaGeneSetApp/>
             </div>)
     }
