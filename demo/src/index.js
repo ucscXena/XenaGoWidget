c7102823c9945153436d3e6c125edd252acfaed6
Nathan Dunn <ndunn@me.com>
2019-03-13 16:48:11 -0700

    updated

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,11 +4,19 @@ import {render} from 'react-dom'
 import PureComponent from "../../src/components/PureComponent";
 import XenaGeneSetApp from "../../src/components/XenaGeneSetApp";
 import {Helmet} from "react-helmet";
+import ReactGA from 'react-ga';
 
+
+function initializeReactGA() {
+    ReactGA.initialize('UA-136203053-1');
+    ReactGA.pageview('/');
+}
 
 class Demo extends PureComponent {
 
+
     render() {
+        initializeReactGA();
         return (
             <div>
                 <Helmet>
