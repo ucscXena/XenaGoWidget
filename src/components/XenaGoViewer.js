e5eac8cd0527bf2010eb567b0a0faa0526dbcb56
Nathan Dunn <ndunn@me.com>
2019-06-19 16:09:03 -0700

    removed an errant piece of code

--- src/components/XenaGoViewer.js
+++ src/components/XenaGoViewer.js
@@ -465,7 +465,6 @@ export default class XenaGoViewer extends PureComponent {
                                                    colorSettings={this.props.colorSettings}
                                                    collapsed={this.props.collapsed}
                                                    viewType={viewType}
-                                                   showClusterSort={this.props.showClusterSort}
                                 />
                             </td>
                             }
