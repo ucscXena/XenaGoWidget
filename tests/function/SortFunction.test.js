0a088652a9451bf69768bf08d74df1f1ecbaa453
Nathan Dunn <ndunn@me.com>
2019-07-13 09:00:51 -0700

    added cluster sort to work

--- tests/function/SortFunction.test.js
+++ tests/function/SortFunction.test.js
@@ -13,6 +13,9 @@ import {
 import ScoredDataInput1 from '../data/ScoreColumnDataInput1';
 import ScoredOutputData1 from '../data/ScoreColumnDataOutput1';
 
+import ClusterSortInput1 from '../data/ClusterSortInput1';
+import ClusterSortOutputData1 from '../data/ClusterSortOutput1';
+
 describe('Sort Functions', () => {
   let node;
 
@@ -28,28 +31,28 @@ describe('Sort Functions', () => {
     let input = [[{"total":1,"mutation":0,"cnv":1,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":1,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0}]];
     let output = [[{"total":1,"mutation":0,"cnv":1,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":1,"cnvLow":0}],[{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0}]];
     expect(output).toEqual(transpose(input))
-  })
+  });
 
   it('Score columns', () => {
     expect(ScoredOutputData1).toEqual(scoreColumns(ScoredDataInput1))
-  })
+  });
 
   it('Sort by type', () => {
     let input = [[{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},"TCGA-OR-A5J1-01"],[{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},"TCGA-OR-A5J4-01"],[{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},"TCGA-OR-A5J7-01"],[{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},"TCGA-OR-A5J9-01"],[{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},{"total":0,"mutation":0,"cnv":0,"mutation4":0,"mutation3":0,"mutation2":0,"cnvHigh":0,"cnvLow":0},"TCGA-OR-A5JA-01"]]
     let output = [ [ { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, "TCGA-OR-A5J1-01" ], [ { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, "TCGA-OR-A5J4-01" ], [ { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, "TCGA-OR-A5J7-01" ], [ { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0 }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, "TCGA-OR-A5J9-01" ], [ { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, { "cnv": 0, "cnvHigh": 0, "cnvLow": 0, "mutation": 0, "mutation2": 0, "mutation3": 0, "mutation4": 0, "total": 0, }, "TCGA-OR-A5JA-01" ] ];
     expect(output).toEqual(sortByType(input))
-  })
+  });
 
   it('Cluster sort', () => {
     expect(clusterSort([0.8,0.05])).toEqual([[0.8],[0.05]])
-  })
+  });
 
   it('Diff sort', () => {
     expect(diffSort([0.8,0.05])).toEqual([[0.8],[0.05]])
-  })
+  });
 
   it('Cluster sample sort', () => {
-    expect(clusterSort([0.8,0.05])).toEqual([[0.8],[0.05]])
+    expect(ClusterSortOutputData1).toEqual(clusterSort(ClusterSortInput1))
   })
   it('Synchrozine sort', () => {
     expect(synchronizedSort([0.8,0.05])).toEqual([[0.8],[0.05]])
