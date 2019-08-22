28040e50114ed34eeab7a0615860f5fa3207f9e4
Nathan Dunn <ndunn@me.com>
2019-08-22 13:05:23 -0700

    cleaned up stlying a little bit

--- src/components/VerticalLegend.js
+++ src/components/VerticalLegend.js
@@ -6,12 +6,19 @@ export default class VerticalLegend extends PureComponent {
 
   render() {
     return (
-      <div style={{margin: 5,padding: 5,fontFamily:'Arial',fontSize:'small', boxShadow: '1px 1px 1px 1px gray',borderRadius:5}}>
-        <span className={BaseStyle.overRepresentedColor}><strong>Over Represented</strong></span>
-        <br/>
-        <span className={BaseStyle.neutralColor}><strong>Occurs as Expected</strong></span>
-        <br/>
-        <span className={BaseStyle.underRepresentedColor}><strong>Under Represented</strong></span>
+      <div>
+      <table className={BaseStyle.verticalLegendBox}>
+          <tbody>
+          <tr><td className={BaseStyle.overRepresentedColor}>
+            <strong>Over Represented</strong>
+          </td></tr>
+            <tr>
+              <td className={BaseStyle.neutralColor}><strong>Occurs as Expected</strong>
+            </td></tr>
+            <tr><td className={BaseStyle.underRepresentedColor}><strong>Under Represented</strong>
+            </td></tr>
+          </tbody>
+        </table>
       </div>
     )
   }
