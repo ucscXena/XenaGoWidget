50b6edb6871096c0f8fcc0846dcbb9b70afb290a
Nathan Dunn <ndunn@me.com>
2019-08-29 13:46:26 -0700

    added a show only option

--- src/components/SubCohortSelector.js
+++ src/components/SubCohortSelector.js
@@ -5,6 +5,10 @@ import Dialog from 'react-toolbox/lib/dialog';
 import Checkbox from 'react-toolbox/lib/checkbox';
 import {isEqual} from 'underscore';
 import {Button} from 'react-toolbox/lib/button';
+import Link from 'react-toolbox/lib/link';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+import Grid from 'react-bootstrap/lib/Grid';
 
 export class SubCohortSelector extends PureComponent {
 
@@ -28,107 +32,127 @@ export class SubCohortSelector extends PureComponent {
     }
   }
 
-    handleChange = (value,field) => {
-      let newSelected = JSON.parse(JSON.stringify(this.state.selectedSubCohorts)) ;
-      if(value){
-        newSelected.push(field);
-      }
-      else{
-        let indexValue = newSelected.indexOf(field);
-        newSelected.splice(indexValue,1);
-      }
-      let allSelected = isEqual(this.props.subCohortsForSelected.sort(),newSelected.sort()) ;
-      this.setState({
-        selectedSubCohorts:newSelected,
-        allSelected,
-      });
-    };
+  handleSelectOnly = (field) => {
+    const newSelected = [field];
+    // this is going to be almost always false
+    let allSelected = isEqual(this.props.subCohortsForSelected.sort(),newSelected.sort()) ;
+    this.setState({
+      selectedSubCohorts:newSelected,
+      allSelected,
+    });
+  };
 
-    selectAll(){
-      this.setState({
-        selectedSubCohorts:this.props.subCohortsForSelected,
-        allSelected:true,
-      });
-      this.props.handleSubCohortChange(this.props.subCohortsForSelected);
+  handleChange = (value,field) => {
+    let newSelected = JSON.parse(JSON.stringify(this.state.selectedSubCohorts)) ;
+    if(value){
+      newSelected.push(field);
     }
-
-    updateSubCategories(){
-      this.props.handleSubCohortChange(this.state.selectedSubCohorts);
+    else{
+      let indexValue = newSelected.indexOf(field);
+      newSelected.splice(indexValue,1);
     }
+    let allSelected = isEqual(this.props.subCohortsForSelected.sort(),newSelected.sort()) ;
+    this.setState({
+      selectedSubCohorts:newSelected,
+      allSelected,
+    });
+  };
 
-    cancelUpdate(){
-      this.setState({
-        selectedSubCohorts:this.state.originalSelectedSubCohorts,
-      });
-      this.props.handleSubCohortChange(this.state.originalSelectedSubCohorts);
-    }
+  selectAll(){
+    this.setState({
+      selectedSubCohorts:this.props.subCohortsForSelected,
+      allSelected:true,
+    });
+    // this.props.handleSubCohortChange(this.props.subCohortsForSelected);
+  }
 
-    render() {
+  updateSubCategories(){
+    this.props.handleSubCohortChange(this.state.selectedSubCohorts);
+  }
 
-      let {active, onToggle,subCohortsForSelected,cohortLabel,selectedCohort} = this.props;
-      let {allSelected,selectedSubCohorts} = this.state ;
+  cancelUpdate(){
+    this.setState({
+      selectedSubCohorts:this.state.originalSelectedSubCohorts,
+    });
+    this.props.handleSubCohortChange(this.state.originalSelectedSubCohorts);
+  }
 
-      return (
-        <Dialog
-          active={active}
-          onEscKeyDown={onToggle}
-          onOverlayClick={onToggle}
-          title='Edit Sub-Cohorts'
-        >
-          <table style={{ width:'100%'}}>
-            <tbody>
-              <tr>
-                <td>
+  render() {
+
+    let {active, onToggle,subCohortsForSelected,cohortLabel,selectedCohort} = this.props;
+    let {allSelected,selectedSubCohorts} = this.state ;
+
+    return (
+      <Dialog
+        active={active}
+        onEscKeyDown={onToggle}
+        onOverlayClick={onToggle}
+        title='Edit Sub-Cohorts'
+      >
+        <table style={{ width:'100%'}}>
+          <tbody>
+            <tr>
+              <td>
                             ({cohortLabel}) Select Sub Cohorts for {selectedCohort.name}
-                </td>
-              </tr>
-              <tr>
-                <td>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Grid style={{marginTop: 20,width:900}}>
                   {
                     subCohortsForSelected.sort().map( cs =>{
                       return (
-                        <Checkbox
-                          checked={selectedSubCohorts.indexOf(cs)>=0} disabled={selectedSubCohorts.length<2 && selectedSubCohorts.indexOf(cs)>=0}
-                          key={cs}
-                          label={cs}
-                          onChange={(value) => this.handleChange(value,cs)}
-                        />
+                        <Row key={cs}>
+                          <Col md={12}>
+                            <input
+                              checked={selectedSubCohorts.indexOf(cs)>=0}
+                              disabled={selectedSubCohorts.length<2 && selectedSubCohorts.indexOf(cs)>=0} key={cs}
+                              name={cs}
+                              onChange={(value) => this.handleChange(value,cs)}
+                              style={{display:'inline', marginRight:10}}
+                              type='checkbox'
+                            />
+                            <div  style={{display: 'inline'}}>{cs}</div>
+                            <Link href='#' label={'(Select Only)'} onClick={() => { this.handleSelectOnly(cs); }} style={{display:'inline', marginLeft: 20,fontSize: 'small'}}/>
+                          </Col>
+                        </Row>
                       );
                     })
                   }
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <Button
-                    icon='save' label='Save' onClick={() => this.updateSubCategories()} primary
-                    raised
-                  />
-                  {!allSelected &&
+                </Grid>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button
+                  icon='save' label='View' onClick={() => this.updateSubCategories()} primary
+                  raised
+                />
+                {!allSelected &&
                             <Button
                               label={'Select All'} onClick={() => this.selectAll()} primary
                               raised
                             />
-                  }
-                  {allSelected &&
+                }
+                {allSelected &&
                             <Button
-                              disabled label={'Select All'} onClick={() => this.selectAll()} primary
+                              disabled label={'Select All'} primary
                               raised
                             />
-                  }
-                  {/*</td>*/}
-                  {/*<td>*/}
-                  <Button
-                    icon='cancel' label='Cancel' onClick={() => this.cancelUpdate()}
-                    raised
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </Dialog>
-      );
-    }
+                }
+                {/*</td>*/}
+                {/*<td>*/}
+                <Button
+                  icon='cancel' label='Cancel' onClick={() => this.cancelUpdate()}
+                  raised
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Dialog>
+    );
+  }
 }
 
 SubCohortSelector.propTypes = {
