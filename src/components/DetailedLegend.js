35041ac00a460480c560b3bac125bbd56b05a7bc
Nathan Dunn <ndunn@me.com>
2019-09-02 08:27:12 -0700

    labeld the legend

--- src/components/DetailedLegend.js
+++ src/components/DetailedLegend.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import PureComponent from './PureComponent';
 import BaseStyle from '../css/base.css';
+import {CardText, Card} from 'react-toolbox';
 
 export class DetailedLegend extends PureComponent {
 
   render() {
     return (
-      <div style={{margin: 5,padding: 5,fontFamily:'Arial',fontSize:'small', boxShadow: '1px 1px 1px 1px gray',borderRadius:5}}>
-        <span className={BaseStyle.cnvHighColor}><strong>CNV Amplification</strong></span>
-        <br/>
-        <span className={BaseStyle.cnvLowColor}><strong>CNV Deletion</strong></span>
-        <hr/>
-        <span className={BaseStyle.mutation4Color}><strong>Deleterious</strong></span>
-                &nbsp;
-        <span className={BaseStyle.mutation3Color}><strong>Splice</strong></span>
-        <br/>
-        <span className={BaseStyle.mutation2Color}><strong>Missense / Inframe</strong></span>
-      </div>
+      <Card>
+        <CardText>
+          <div style={{marginBottom: 5,fontSize: 'large',fontWeight:'bolder'}}>Legend</div>
+          <span className={BaseStyle.cnvHighColor}><strong>CNV Amplification</strong></span>
+          <br/>
+          <span className={BaseStyle.cnvLowColor}><strong>CNV Deletion</strong></span>
+          <hr/>
+          <span className={BaseStyle.mutation4Color}><strong>Deleterious</strong></span>
+          &nbsp;
+          <span className={BaseStyle.mutation3Color}><strong>Splice</strong></span>
+          <br/>
+          <span className={BaseStyle.mutation2Color}><strong>Missense / Inframe</strong></span>
+        </CardText>
+      </Card>
     );
   }
 }
