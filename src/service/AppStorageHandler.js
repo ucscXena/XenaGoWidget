caeddafc3dfd09d95df48b09ec42dec895be1e0e
Nathan Dunn <ndunn@me.com>
2019-09-03 09:41:34 -0700

    changed default filter to CNV_MUTATION instead of all

--- src/service/AppStorageHandler.js
+++ src/service/AppStorageHandler.js
@@ -3,6 +3,7 @@ import PureComponent from '../components/PureComponent';
 import DefaultPathWays from '../data/genesets/tgac';
 import { SortType } from '../functions/SortFunctions';
 import {fetchCohortData, getSubCohortsOnlyForCohort} from '../functions/CohortFunctions';
+import {FILTER_ENUM} from "../components/FilterSelector";
 
 
 // synchronizing gene sorts between pathways
@@ -139,7 +140,7 @@ export class AppStorageHandler extends PureComponent {
     if (appState && appState.filterState && appState.filterState[cohortIndex]) {
       return appState.filterState[cohortIndex];
     }
-    return 'All';
+    return FILTER_ENUM.CNV_MUTATION;
   }
 
   static getSortState() {
