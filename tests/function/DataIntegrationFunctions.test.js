89668e362be6c6c2ce9842e89371e9db385e2154
Nathan Dunn <ndunn@me.com>
2019-09-03 15:36:54 -0700

    fixed gene expression tests

--- tests/function/DataIntegrationFunctions.test.js
+++ tests/function/DataIntegrationFunctions.test.js
@@ -75,6 +75,7 @@ import {FILTER_ENUM} from '../../src/components/FilterSelector';
 
 describe('Data Integration Functions', () => {
 
+  // TODO: reactivate
   // it('Associated Data', () => {
   //   expect(AssociatedDataOutput1).toEqual(doDataAssociations(AssociatedDataExpression1, AssociatedDataCopyNumber1, AssociatedDataGeneList1, AssociatedDataPathways1, AssociatedDataSamples1, FILTER_ENUM.CNV_MUTATION));
   // });
@@ -130,6 +131,7 @@ describe('Data Integration Functions', () => {
     expect(outputFile).toEqual(FilterBothOutput1);
   });
 
+  // TODO: reactivate
   // it('Find Associated Data', () => {
   //   expect(FindAssociatedDataOutput1).toEqual(findAssociatedData(FindAssociatedDataInputHash1,FindAssociatedDataKey1));
   // });
@@ -174,6 +176,7 @@ describe('Data Integration Functions', () => {
   // TODO: note for some reason this triggers:
   // ERROR: 'Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in %s.%s', 'the componentWillUnmount method', '
   // TODO: probably because its calculating based on the fetch
+  // TODO: activate this again
   // it('Calculate All Pathways', () => {
   //   expect(calculateAllPathways([CalculateAllPathwaysA,CalculateAllPathwaysB])).toEqual(CalculateAllPathwaysOutput);
   // });
