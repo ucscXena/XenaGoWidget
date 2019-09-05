31da550ba30aac455b91047d924a0495efd91f58
Nathan Dunn <ndunn@me.com>
2019-09-04 18:32:13 -0700

    added hooks back but everything is breaking

--- src/functions/FetchFunctions.js
+++ src/functions/FetchFunctions.js
@@ -54,7 +54,7 @@ export function calculateSubCohortSamples(availableSamples, cohort){
   }
 }
 
-function getSamplesForFilter( mutationSamples,copyNumberSamples, filter){
+function getSamplesForFilter( mutationSamples,copyNumberSamples,geneExpressionSamples, filter){
   switch (filter) {
   case FILTER_ENUM.CNV_MUTATION:
     return uniq(intersection(mutationSamples, copyNumberSamples));
@@ -62,6 +62,8 @@ function getSamplesForFilter( mutationSamples,copyNumberSamples, filter){
     return mutationSamples;
   case FILTER_ENUM.COPY_NUMBER:
     return copyNumberSamples;
+  case FILTER_ENUM.GENE_EXPRESSION:
+    return geneExpressionSamples;
   default:
     // eslint-disable-next-line no-console
     console.error('invalid filter', filter);
@@ -77,26 +79,36 @@ export function fetchCombinedCohorts(selectedCohorts, pathways,filter, combinati
   Rx.Observable.zip(
     datasetSamples(selectedCohorts[0].host, selectedCohorts[0].mutationDataSetId, null),
     datasetSamples(selectedCohorts[0].host, selectedCohorts[0].copyNumberDataSetId, null),
+    datasetSamples(selectedCohorts[0].geneExpression.host, selectedCohorts[0].geneExpression.dataset, null),
+    // TODO: add gene expression 0
     datasetSamples(selectedCohorts[1].host, selectedCohorts[1].mutationDataSetId, null),
     datasetSamples(selectedCohorts[1].host, selectedCohorts[1].copyNumberDataSetId, null),
+    datasetSamples(selectedCohorts[1].geneExpression.host, selectedCohorts[1].geneExpression.dataset, null),
+    // TODO: add gene expression 1
   ). flatMap((unfilteredSamples) => {
-    // TODO: pass in filter count somehow
+    // TODO: add gene expression with the second one
     filterCounts = [createFilterCounts(unfilteredSamples[0],unfilteredSamples[1]),createFilterCounts(unfilteredSamples[2],unfilteredSamples[3])];
     // with all of the samples, we can now provide accurate numbers, maybe better to store on the server, though
-    // const geneExpression =
     // merge based on filter
     const availableSamples = [
       calculateSubCohortSamples(unfilteredSamples[0],selectedCohorts[0]),
       calculateSubCohortSamples(unfilteredSamples[1],selectedCohorts[0]),
-      calculateSubCohortSamples(unfilteredSamples[2],selectedCohorts[1]),
-      calculateSubCohortSamples(unfilteredSamples[3],selectedCohorts[1])
+      calculateSubCohortSamples(unfilteredSamples[2],selectedCohorts[0]),
+      // TODO: add gene expression 0
+
+      calculateSubCohortSamples(unfilteredSamples[3],selectedCohorts[1]),
+      calculateSubCohortSamples(unfilteredSamples[4],selectedCohorts[1]),
+      calculateSubCohortSamples(unfilteredSamples[5],selectedCohorts[1]),
+      // TODO: add gene expression 1
     ];
 
     // calculate samples for what samples we will actually fetch
-    const samplesA = getSamplesForFilter(availableSamples[0],availableSamples[1],filter[0]);
-    const samplesB = getSamplesForFilter(availableSamples[2],availableSamples[2],filter[1]);
+    // TODO: add gene expression
+    const samplesA = getSamplesForFilter(availableSamples[0],availableSamples[1],availableSamples[2],filter[0]);
+    const samplesB = getSamplesForFilter(availableSamples[2],availableSamples[3],availableSamples[4],filter[1]);
 
     // TODO: make this a testable function
+    // TODO: minimize fetches based on the filter
     return Rx.Observable.zip(
       sparseData(selectedCohorts[0].host, selectedCohorts[0].mutationDataSetId, samplesA, geneList),
       datasetFetch(selectedCohorts[0].host, selectedCohorts[0].copyNumberDataSetId, samplesA, geneList),
