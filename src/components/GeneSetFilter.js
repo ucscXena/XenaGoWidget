319c220eaea7cfc3d52a21f45c0a0379f395d58d
Nathan Dunn <ndunn@me.com>
2019-10-02 16:25:51 -0700

    fixed scoring

--- src/components/GeneSetFilter.js
+++ src/components/GeneSetFilter.js
@@ -47,7 +47,8 @@ export default class GeneSetFilter extends PureComponent {
 
     let { selectedCohort, samples } = this.state;
 
-    const geneSetLabels = convertPathwaysToGeneSetLabel(this.props.pathways).slice(0,100);
+    // const geneSetLabels = convertPathwaysToGeneSetLabel(this.props.pathways).slice(0,100);
+    const geneSetLabels = convertPathwaysToGeneSetLabel(LargePathways).slice(0,100);
     // const geneSetLabels = convertPathwaysToGeneSetLabel(this.props.pathways);
 
     console.log('query with',samples[0].length,samples[1].length,geneSetLabels.length);
