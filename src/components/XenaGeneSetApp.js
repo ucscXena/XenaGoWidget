ecffe0f03bd5d1f05032428e3c6fdf4a1f12009e
Nathan Dunn <ndunn@me.com>
2019-11-01 15:52:09 -0700

    added the proper onchange event

--- src/components/XenaGeneSetApp.js
+++ src/components/XenaGeneSetApp.js
@@ -707,7 +707,7 @@ export default class XenaGeneSetApp extends PureComponent {
                           <br/>
                           Limit <input onChange={(event) => this.setState({geneSetLimit:event.target.value} )} size={3} value={this.state.geneSetLimit}/>
                           Filter Gene Sets by
-                          <select>
+                          <select onChange={(event) => this.setState({filterBy:event.target.value})}>
                             <option selected={this.state.filterBy==='AbsDiff'} value='AbsDiff'>Abs Diff BPA</option>
                             <option selected={this.state.filterBy==='Diff'} value='Diff'>Cohort Diff BPA</option>
                             <option selected={this.state.filterBy==='Total'} value='Total'>Total BPA</option>
@@ -720,7 +720,7 @@ export default class XenaGeneSetApp extends PureComponent {
                           }
                           <br/>
                           Sort Visible Gene Sets by
-                          <select>
+                          <select onChange={(event) => this.setState({sortViewBy:event.target.value})}>
                             <option selected={this.state.sortViewBy==='AbsDiff'} value='AbsDiff'>Abs Diff BPA</option>
                             <option selected={this.state.sortViewBy==='Diff'} value='Diff'>Cohort Diff BPA</option>
                             <option selected={this.state.sortViewBy==='Total'} value='Total'>Total BPA</option>
