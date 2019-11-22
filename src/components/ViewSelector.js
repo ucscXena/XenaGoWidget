990ee28e3449d151a93d8f692a99f12b9940aa1a
Nathan Dunn <ndunn@me.com>
2019-11-21 18:56:37 -0800

    removed filter counts

--- src/components/ViewSelector.js
+++ src/components/ViewSelector.js
@@ -2,13 +2,8 @@ import React from 'react';
 import PureComponent from './PureComponent';
 import PropTypes from 'prop-types';
 import {Dropdown} from 'react-toolbox';
+import {VIEW_ENUM} from '../data/ViewEnum';
 
-function lowerCaseCompare(a, b) {
-  // put gene expression at the bottom
-  if(a==='Gene Expression') return 1 ;
-  if(b==='Gene Expression') return 1 ;
-  return a.toLowerCase().localeCompare(b.toLowerCase());
-}
 
 export class ViewSelector extends PureComponent {
 
@@ -18,16 +13,11 @@ export class ViewSelector extends PureComponent {
 
     render() {
       const {pathwayData, selected} = this.props;
-      const filterCounts = pathwayData.filterCounts;
       if(pathwayData.expression.length === 0){
         return <div>Loading...</div>;
       }
-      let labels = Object.keys(filterCounts).sort(lowerCaseCompare);
-      const labelValues = labels.map(label => {
-        if(filterCounts[label].current!==filterCounts[label].available){
-          return ({label: label + ' (' + filterCounts[label].current + '/' + filterCounts[label].available+ ')', value: label});
-        }
-        return ({label: label + ' (' + filterCounts[label].current +')', value: label});
+      const labelValues = Object.values(VIEW_ENUM).map(label => {
+        return ({label: label , value: label});
       });
       return (
         <div style={{marginLeft: 10,marginTop:0,height:65}}>
