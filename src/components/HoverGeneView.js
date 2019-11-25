5faad454e7490ee352f9fd131d55c86ac61adee1
Nathan Dunn <ndunn@me.com>
2019-11-25 15:10:13 -0800

    fixes #465

--- src/components/HoverGeneView.js
+++ src/components/HoverGeneView.js
@@ -38,33 +38,33 @@ export default class HoverGeneView extends PureComponent {
       // return cohortIndex === 0 ? data.pathway.firstGeneExpressionPathwayActivity: data.pathway.secondGeneExpressionPathwayActivity;
       // shows individual samples if available
       if(cohortIndex===0){
-        return data.pathway.firstSampleGeneExpressionPathwayActivity!==undefined ? data.pathway.firstSampleGeneExpressionPathwayActivity: data.pathway.firstGeneExpressionPathwayActivity;
+        return data.pathway.firstSampleGeneExpressionPathwayActivity!==undefined  && data.tissue !=='Header' ? data.pathway.firstSampleGeneExpressionPathwayActivity: data.pathway.firstGeneExpressionPathwayActivity;
       }
       else{
-        return data.pathway.secondSampleGeneExpressionPathwayActivity!==undefined ? data.pathway.secondSampleGeneExpressionPathwayActivity: data.pathway.secondGeneExpressionPathwayActivity;
+        return data.pathway.secondSampleGeneExpressionPathwayActivity!==undefined && data.tissue !=='Header' ? data.pathway.secondSampleGeneExpressionPathwayActivity: data.pathway.secondGeneExpressionPathwayActivity;
       }
     case VIEW_ENUM.REGULON:
       // shows individual samples if available
       if(cohortIndex===0){
-        return data.pathway.firstSampleRegulonPathwayActivity!==undefined ? data.pathway.firstSampleRegulonPathwayActivity: data.pathway.firstRegulonPathwayActivity;
+        return data.pathway.firstSampleRegulonPathwayActivity!==undefined  && data.tissue !=='Header'? data.pathway.firstSampleRegulonPathwayActivity: data.pathway.firstRegulonPathwayActivity;
       }
       else{
-        return data.pathway.secondSampleRegulonPathwayActivity!==undefined ? data.pathway.secondSampleRegulonPathwayActivity: data.pathway.secondRegulonPathwayActivity;
+        return data.pathway.secondSampleRegulonPathwayActivity!==undefined  && data.tissue !=='Header'? data.pathway.secondSampleRegulonPathwayActivity: data.pathway.secondRegulonPathwayActivity;
       }
     case VIEW_ENUM.PARADIGM:
       // shows individual samples if available
       if(cohortIndex===0){
-        return data.pathway.firstSampleParadigmPathwayActivity!==undefined ? data.pathway.firstSampleParadigmPathwayActivity: data.pathway.firstParadigmPathwayActivity;
+        return data.pathway.firstSampleParadigmPathwayActivity!==undefined  && data.tissue !=='Header'? data.pathway.firstSampleParadigmPathwayActivity: data.pathway.firstParadigmPathwayActivity;
       }
       else{
-        return data.pathway.secondSampleParadigmPathwayActivity!==undefined ? data.pathway.secondSampleParadigmPathwayActivity: data.pathway.secondParadigmPathwayActivity;
+        return data.pathway.secondSampleParadigmPathwayActivity!==undefined  && data.tissue !=='Header'? data.pathway.secondSampleParadigmPathwayActivity: data.pathway.secondParadigmPathwayActivity;
       }
     default:
       if(cohortIndex===0){
-        return data.pathway.firstSampleTotal!==undefined ? data.pathway.firstSampleTotal : data.pathway.firstChiSquared;
+        return data.pathway.firstSampleTotal!==undefined  && data.tissue !=='Header'? data.pathway.firstSampleTotal : data.pathway.firstChiSquared;
       }
       else{
-        return data.pathway.secondSampleTotal!==undefined ? data.pathway.secondSampleTotal : data.pathway.secondChiSquared;
+        return data.pathway.secondSampleTotal!==undefined  && data.tissue !=='Header'? data.pathway.secondSampleTotal : data.pathway.secondChiSquared;
       }
     }
   };
