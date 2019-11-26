29c6091ce35aec2089f2dea213017e7a2b078e8c
Nathan Dunn <ndunn@me.com>
2019-11-25 16:14:55 -0800

    merged in changes

--- src/components/SubCohortSelector.js
+++ src/components/SubCohortSelector.js
@@ -15,7 +15,6 @@ export class SubCohortSelector extends PureComponent {
 
   constructor(props) {
     super(props);
-    console.log('props',props,props.filterCounts.subCohortCounts);
     this.state = {
       originalSelectedSubCohorts:props.selectedSubCohorts,
       selectedSubCohorts:props.selectedSubCohorts,
