88b7dd9e317804b1d9ac5adbbab472ce954f3869
Nathan Dunn <ndunn@me.com>
2019-12-19 12:28:01 -0800

    fixes #491

--- src/components/GeneSetInfoBox.js
+++ src/components/GeneSetInfoBox.js
@@ -4,7 +4,15 @@ import PropTypes from 'prop-types';
 import BaseStyle from '../css/base.css';
 // import {IconButton} from 'react-toolbox';
 import {Dialog} from 'react-toolbox/lib';
+import Tooltip from 'react-toolbox/lib/tooltip';
+import Link from 'react-toolbox/lib/link';
+import {
+  getSamplesFromSubCohort
+} from '../functions/CohortFunctions';
 
+const TooltipLink = Tooltip(Link);
+
+const MAGIC_LENGTH = 20 ;
 
 export class GeneSetInfoBox extends PureComponent {
 
@@ -24,29 +32,38 @@ export class GeneSetInfoBox extends PureComponent {
 
     const {cohortIndex,samplesLength,selectedCohort} = this.props;
 
-    // console.log('seleced cohort',selectedCohort);
-
+    const label = selectedCohort.name.length>MAGIC_LENGTH ? selectedCohort.name.substr(0,MAGIC_LENGTH-3)+'..' : selectedCohort.name;
     return (
       <div className={cohortIndex===0 ? BaseStyle.topInfoBox : BaseStyle.bottomInfoBox}>
-        {selectedCohort.name}
-        <br/>
+        <TooltipLink
+          className={BaseStyle.infoLink} href="#" icon='info' label={label}
+          onClick={()=>this.setState({showInfo: true})}
+          tooltip={selectedCohort.name}
+        />
         <Dialog
           active={this.state.showInfo}
           onEscKeyDown={() => this.setState({showInfo:false})}
           onOverlayClick={() => this.setState({showInfo:false})}
-          title="Cohort Info"
-          type='small'
+          title={selectedCohort.name}
+          type='normal'
         >
           <div>
-            {selectedCohort.selectedSubCohorts}
+            <ul>
+              {selectedCohort.selectedSubCohorts.sort().map( s => {
+                return (
+                  <li key={s}>{s} ({getSamplesFromSubCohort(selectedCohort.name,s).length})</li>
+                );
+              }
+              )}
+            </ul>
           </div>
         </Dialog>
 
-        <button className={BaseStyle.samplesBox} onClick={() => alert('show info')} type='button'>
+        <br/>
+        <div className={BaseStyle.samplesBox}>
           { selectedCohort.selectedSubCohorts.length } / {selectedCohort.subCohorts.length } cohorts selected
-        </button>
-
-        {/*<IconButton icon='info' onClick={() => this.setState({showInfo: true})} style={{marginBottom: 5}}/>*/}
+        </div>
+        <br/>
 
         <br/>
         <div className={BaseStyle.samplesBox}>
