c1ce10170228e3435b43cc1172c02ad74de4e321
Nathan Dunn <ndunn@me.com>
2019-12-20 12:27:20 -0800

    moved gene set label view around

--- src/components/XenaGeneSetApp.js
+++ src/components/XenaGeneSetApp.js
@@ -711,14 +711,6 @@ export default class XenaGeneSetApp extends PureComponent {
                   </select>
                 </td>
                 <td>
-                  {/*{this.state.pathwaySelection &&*/}
-                  {/*<div className={BaseStyle.geneSetInfoBox}>*/}
-                  {/*  <div className={BaseStyle.geneSetBoxLabel}>Gene Set</div>*/}
-                  {/*  {this.state.pathwaySelection.pathway.golabel}*/}
-                  {/*      &nbsp;*/}
-                  {/*      ({this.state.pathwaySelection.pathway.gene.length} genes)*/}
-                  {/*</div>*/}
-                  {/*}*/}
                 </td>
               </tr>
               {isViewGeneExpression(this.state.filter) &&
@@ -757,14 +749,6 @@ export default class XenaGeneSetApp extends PureComponent {
                           </div>
                         </td>
                         <td>
-                          {/*{this.state.pathwaySelection &&*/}
-                          {/*<div className={BaseStyle.geneSetInfoBox}>*/}
-                          {/*  <div className={BaseStyle.geneSetBoxLabel}>Gene Set</div>*/}
-                          {/*  {this.state.pathwaySelection.pathway.golabel}*/}
-                          {/*  &nbsp;*/}
-                          {/*  ({this.state.pathwaySelection.pathway.gene.length} genes)*/}
-                          {/*</div>*/}
-                          {/*}*/}
                         </td>
                       </tr>
               }
@@ -781,7 +765,7 @@ export default class XenaGeneSetApp extends PureComponent {
 
                   {this.state.pathwaySelection &&
                   <div className={BaseStyle.geneSetInfoBox}>
-                    <div className={BaseStyle.geneSetBoxLabel}>Gene Set</div>
+                    <div className={BaseStyle.geneSetBoxLabel}>Viewing Gene Set Details</div>
                     {this.state.pathwaySelection.pathway.golabel}
                     &nbsp;
                     ({this.state.pathwaySelection.pathway.gene.length} genes)
