9e9e265f4f733b0350542fef4a384ef64472a982
Nathan Dunn <ndunn@me.com>
2020-02-02 09:26:32 -0800

    added a legend

--- src/components/GeneSetLegend.js
+++ src/components/GeneSetLegend.js
@@ -2,11 +2,20 @@ import React from 'react';
 import PureComponent from './PureComponent';
 import PropTypes from 'prop-types';
 
+const TEXT_Y_OFFSET = 15;
+const IMAGE_HEIGHT = 10 ;
+// const IMAGE_WIDTH = 15;
+
 export class GeneSetLegend extends PureComponent {
 
+
   render() {
+
+    const formattedMin = this.props.minScore.toFixed(2);
+    const formattedMax = this.props.maxScore.toFixed(2);
+
     return (
-      <svg height="40" width="100%">
+      <svg height="20" width="100%">
         <defs>
           <linearGradient id="grad1">
             <stop offset="0%" stopColor="red" />
@@ -14,15 +23,12 @@ export class GeneSetLegend extends PureComponent {
             <stop offset="100%" stopColor="blue" />
           </linearGradient>
         </defs>
-        <rect
-          fill="url(#grad1)" height={20} width={300} x={0}
-          y={0}
-        />
-        <text height={20} width={300} x={10} y={25}>
-          {this.props.maxScore}
+        <text fontFamily='monospace' height={20} width={20} x={0} y={TEXT_Y_OFFSET}>
+          {formattedMax}
         </text>
-        <text height={20} width={300} x={80} y={25}>
-          {this.props.minScore}
+        <rect fill="url(#grad1)" height={IMAGE_HEIGHT} width={50} x={40} y={5}/>
+        <text fontFamily='monospace' height={10} width={300} x={100} y={TEXT_Y_OFFSET}>
+          {formattedMin}
         </text>
       </svg>
     );
--- src/components/XenaGeneSetApp.js
+++ src/components/XenaGeneSetApp.js
@@ -734,7 +734,7 @@ export default class XenaGeneSetApp extends PureComponent {
               />
             </Dialog>
           }
-          <table border="solid 2px black">
+          <table>
             <tbody>
               {isViewGeneExpression(this.state.filter) &&
                       <tr>
@@ -804,15 +804,6 @@ export default class XenaGeneSetApp extends PureComponent {
                         </td>
                       </tr>
               }
-
-              <tr>
-                <td colSpan={3}>
-                  <GeneSetLegend maxScore={maxValue} minScore={-maxValue}/>
-                </td>
-                <td colSpan={2}>
-                  <GeneSetLegend maxScore={2} minScore={-2}/>
-                </td>
-              </tr>
               <tr>
                 <td colSpan={3} width={VERTICAL_GENESET_DETAIL_WIDTH*2 + VERTICAL_GENESET_DETAIL_WIDTH}>
                   <DetailedLabelTop
@@ -822,7 +813,6 @@ export default class XenaGeneSetApp extends PureComponent {
                   />
                 </td>
                 <td>
-
                   {this.state.pathwaySelection &&
                   <div className={BaseStyle.geneSetInfoBox}>
                     <div className={BaseStyle.geneSetBoxLabel}>Viewing Gene Set Details</div>
@@ -831,11 +821,19 @@ export default class XenaGeneSetApp extends PureComponent {
                     ({this.state.pathwaySelection.pathway.gene.length} genes)
                   </div>
                   }
-
-
                 </td>
-
-
+              </tr>
+              <tr>
+                <td colSpan={3}>
+                  <div style={{marginLeft:230}}>
+                    <GeneSetLegend maxScore={maxValue} minScore={-maxValue}/>
+                  </div>
+                </td>
+                <td colSpan={1}>
+                  <div  style={{marginLeft:250}}>
+                    <GeneSetLegend maxScore={2} minScore={-2}/>
+                  </div>
+                </td>
               </tr>
               <tr>
                 <td>
