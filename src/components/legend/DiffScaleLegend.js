ce77622c6449292268cc3d76824702453ac36698
Nathan Dunn <ndunn@me.com>
2020-05-21 13:58:18 -0700

    fixed scale nad heights

--- src/components/legend/DiffScaleLegend.js
+++ src/components/legend/DiffScaleLegend.js
@@ -3,11 +3,19 @@ import PureComponent from '../PureComponent'
 // import {GeneSetLegend} from './GeneSetLegend'
 // import {VIEW_ENUM} from '../../data/ViewEnum'
 import PropTypes from 'prop-types'
+import {VERTICAL_GENESET_DETAIL_WIDTH} from '../XenaGeneSetApp'
 // import {GeneLegendLabel} from './GeneLegendLabel'
 // import BaseStyle from '../../css/base.css'
 
 
 export class DiffScaleLegend extends PureComponent {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      showScale : props.showScale
+    }
+  }
 
   render() {
     if(!this.props.showScale){
@@ -21,42 +29,55 @@ export class DiffScaleLegend extends PureComponent {
       <tr style={{height: 20}} >
         <td colSpan={1}>
           <svg style={{width: '100%', height: 20, borderColor: 'black', borderWidth: 1 }}>
-            <rect
-              height={20}
-              key={1}
-              style={{fill:'none',strokeWidth:3,stroke:'black'}}
-              width={'100%'}
-              x={0}
-              y={0}
+            <polyline
+              fill={'none'}
+              points={`0,0 0,20 0,15 ${VERTICAL_GENESET_DETAIL_WIDTH},15  ${VERTICAL_GENESET_DETAIL_WIDTH},0   ${VERTICAL_GENESET_DETAIL_WIDTH},20`}
+              stroke={'black'}
+              strokeWidth={1}
             />
-
+            <text
+              fontSize={'smaller'}
+              x={10}
+              y={10}
+            >
+              {this.props.maxValue}
+            </text>
+            <text
+              fontSize={'smaller'}
+              x={VERTICAL_GENESET_DETAIL_WIDTH - 20}
+              y={10}
+            >
+              {this.props.minValue}
+            </text>
           </svg>
-          {/*<GeneLegendLabel/>*/}
         </td>
         <td colSpan={1} style={{height: 20}}>
-          <pre style={{display: 'inline',padding: 0, margin: 0, height: 20}}>Gene Diff Scale</pre>
-          <input style={{display: 'inline'}} type='checkbox'/> Show
-          {/*<div className={BaseStyle.legendTextDiv}>*/}
-          {/*  <pre style={{marginLeft: 0,display:'inline'}}>{getMiddleGeneLabelForView(this.props.filter)}</pre>*/}
-          {/*  <br/>*/}
-          {/*  <GeneSetLegend*/}
-          {/*    id='geneExpressionGeneScore'*/}
-          {/*    maxScore={2}*/}
-          {/*    minScore={-2}*/}
-          {/*  />*/}
-          {/*</div>*/}
+          <pre style={{display: 'inline',paddingLeft: 10,paddingBottom: 0,paddingTop: 0, margin: 0, height: 20}}>Gene Diff Scale</pre>
+          <input checked={this.state.showScale} style={{display: 'inline'}} type='checkbox'/> Show
         </td>
         <td colSpan={1}>
-          {/*<div className={BaseStyle.legendTextDiv}>*/}
-          {/*  <pre style={{marginLeft: 0,display:'inline'}}>{getSampleGeneLabelForView(this.props.filter)}</pre>*/}
-          {/*  <br/>*/}
-          {/*  <GeneSetLegend*/}
-          {/*    id='geneExpressionGeneSampleScore'*/}
-          {/*    // label={this.props.filter + ' score'}*/}
-          {/*    maxScore={2}*/}
-          {/*    minScore={-2}*/}
-          {/*  />*/}
-          {/*</div>*/}
+          <svg style={{width: '100%', height: 20, borderColor: 'black', borderWidth: 1 }}>
+            <polyline
+              fill={'none'}
+              points={`0,0 0,20 0,15 ${VERTICAL_GENESET_DETAIL_WIDTH},15  ${VERTICAL_GENESET_DETAIL_WIDTH},0   ${VERTICAL_GENESET_DETAIL_WIDTH},20`}
+              stroke={'black'}
+              strokeWidth={1}
+            />
+            <text
+              fontSize={'smaller'}
+              x={10}
+              y={10}
+            >
+              {-this.props.minValue}
+            </text>
+            <text
+              fontSize={'smaller'}
+              x={VERTICAL_GENESET_DETAIL_WIDTH - 30}
+              y={10}
+            >
+              {-this.props.maxValue}
+            </text>
+          </svg>
         </td>
       </tr>
     )
