3118a2923969e7928f5404df21f91a2b0d444091
Nathan Dunn <ndunn@me.com>
2020-11-23 16:37:17 -0800

    left and right are aligned now

--- src/components/XenaGeneSetApp.js
+++ src/components/XenaGeneSetApp.js
@@ -1115,7 +1115,7 @@ export default class XenaGeneSetApp extends PureComponent {
             const x = ev.clientX + 9
             const y = ev.clientY + 275 - topClient
             // if (    ((x >= 265 && x <= 445) || (x >= 673 && x <= 853)) ) {
-            if ( x >= 265 && x <= 853 ) {
+            if ( x >= 275 && x <= 865 ) {
               this.setState({mousing: true, x, y})
             } else {
               this.setState({mousing: false, x, y})
