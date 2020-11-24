27bd9b5aea44a2f5b14561f068bbee5bd34a1ad2
Nathan Dunn <nathandunn@lbl.gov>
2020-11-23 20:07:55 -0800

    fixes overlap (#648)
    * Y is now aligned correctly
    
    * left and right are aligned now

--- src/components/crosshair/CrossHairH.js
+++ src/components/crosshair/CrossHairH.js
@@ -15,7 +15,7 @@ export default class CrossHairH extends PureComponent{
 
   render() {
     let {mousing, y} = this.props
-    const isVisible = mousing && y > 305
+    const isVisible = mousing && y > 270
     return (
       <div className={BaseStyle.crosshairH} style={{top: y, visibility: isVisible ? 'visible' : 'hidden'}}/>
     )
