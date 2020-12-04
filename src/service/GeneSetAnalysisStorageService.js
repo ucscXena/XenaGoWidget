aaf204d0d025c75537d9ef08775fd3de6c4d39aa
Nathan Dunn <ndunn@me.com>
2020-12-04 15:00:07 -0800

    removed static load states and console logs

--- src/service/GeneSetAnalysisStorageService.js
+++ src/service/GeneSetAnalysisStorageService.js
@@ -5,6 +5,7 @@ export async function getAllCustomGeneSets(){
     const {data} = await axios.get('http://localhost:3001/geneset')
     return data
   } catch (e) {
+    // eslint-disable-next-line no-console
     console.error(e)
     return []
   }
