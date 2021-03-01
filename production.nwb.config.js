c1a6aa76bfb3654360d9c9c90c5a25e3343ac9dc
Nathan Dunn <nathandunn@lbl.gov>
2021-03-01 10:39:51 -0800

    added production configs

--- production.nwb.config.js
+++ production.nwb.config.js
@@ -24,6 +24,7 @@ module.exports = {
   webpack: {
     define:{
       __SERVER__:JSON.stringify('https://xenageneset.berkeleybop.io/xena/'),
+      __GOOGLE_APP_ID__:JSON.stringify('654629507592-7fbpjgsomq6g1lofno7p73qkjqrt62c9.apps.googleusercontent.com'),
       __VERSION__: JSON.stringify(require('./package.json').version)
     },
     styles: {
