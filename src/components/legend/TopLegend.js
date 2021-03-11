4db1428c1d28dab4af950e7e2b62fe78633757d6
Nathan Dunn <nathandunn@lbl.gov>
2021-03-10 17:04:05 -0800

    fixes #639

--- src/components/legend/TopLegend.js
+++ src/components/legend/TopLegend.js
@@ -12,10 +12,14 @@ export class TopLegend extends PureComponent {
 
     return (
       <tr className={BaseStyle.middleSampleLegend}>
-        <td colSpan={1} width={DETAIL_WIDTH}/>
-        <td colSpan={1} width={LABEL_WIDTH}>
+        <td colSpan={1} width={DETAIL_WIDTH}>
           <FaArrowDown style={{marginLeft:60}}/>
-          <span className={BaseStyle.legendLabel}>Middle</span>
+          <span className={BaseStyle.legendLabel}>Sample</span>
+          <FaArrowDown/>
+        </td>
+        <td colSpan={1} width={LABEL_WIDTH}>
+          <FaArrowDown style={{marginLeft:40}}/>
+          <span className={BaseStyle.legendLabel}>Gene Set Summary</span>
           <FaArrowDown/>
         </td>
         <td colSpan={1} width={DETAIL_WIDTH}>
