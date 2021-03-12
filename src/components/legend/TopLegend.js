4ccfffb0f0f389942879a48c7a52f5e7cd692439
Nathan Dunn <nathandunn@lbl.gov>
2021-03-12 11:45:14 -0800

    udpated

--- src/components/legend/TopLegend.js
+++ src/components/legend/TopLegend.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PureComponent from '../PureComponent'
 import BaseStyle from '../../css/base.css'
-import FaArrowDown from 'react-icons/lib/fa/arrow-down'
+// import FaArrowDown from 'react-icons/lib/fa/arrow-down'
 import {DETAIL_WIDTH, LABEL_WIDTH} from '../XenaGeneSetApp'
 
 
@@ -13,19 +13,19 @@ export class TopLegend extends PureComponent {
     return (
       <tr className={BaseStyle.middleSampleLegend}>
         <td colSpan={1} width={DETAIL_WIDTH}>
-          <FaArrowDown style={{marginLeft:60}}/>
+          {/*<FaArrowDown style={{marginLeft:60}}/>*/}
           <span className={BaseStyle.legendLabel}>Samples</span>
-          <FaArrowDown/>
+          {/*<FaArrowDown/>*/}
         </td>
         <td colSpan={1} width={LABEL_WIDTH}>
-          <FaArrowDown style={{marginLeft:40}}/>
+          {/*<FaArrowDown style={{marginLeft:40}}/>*/}
           <span className={BaseStyle.legendLabel}>Gene Set Summary</span>
-          <FaArrowDown/>
+          {/*<FaArrowDown/>*/}
         </td>
         <td colSpan={1} width={DETAIL_WIDTH}>
-          <FaArrowDown style={{marginLeft:60}}/>
+          {/*<FaArrowDown style={{marginLeft:60}}/>*/}
           <span className={BaseStyle.legendLabel}>Samples</span>
-          <FaArrowDown/>
+          {/*<FaArrowDown/>*/}
         </td>
       </tr>
     )
